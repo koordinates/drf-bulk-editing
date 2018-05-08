6f023e0c11942f8a5f22a60b48bd48914b656922
Craig de Stigter <craig.destigter@koordinates.com>
2018-05-09 10:43:18 +1200

    v0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drf-bulk-editing',
-    version='0.0.3',
+    version='0.0.4',
     url='http://www.django-rest-framework.org',
     license='BSD',
     description=open('README.md', 'r').read(),
